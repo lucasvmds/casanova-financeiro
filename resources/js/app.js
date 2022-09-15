335987d936bf3c29e56feffd31b06e3294cabd4c
Lucas Moreira dos Santos <lucasmdsandradina@outlook.com>
2022-09-15 22:01:23 +0000

    Configurar projeto para trabalhar com Inertia + Svelte

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
-import './bootstrap';
+// import './bootstrap';
+import { createInertiaApp } from '@inertiajs/inertia-svelte'
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import "../less/app.less";
+
+createInertiaApp({
+    resolve: name => resolvePageComponent(`../svelte/Pages/${name}.svelte`, import.meta.glob('../svelte/**/*.svelte')),
+    setup({ el, App, props }) {
+        new App({ target: el, props })
+    },
+});
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,24 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            input: 'resources/js/app.js',
+            refresh: [
+                'resources/**',
+            ],
+        }),
+        svelte({
+            experimental: { prebundleSvelteLibraries: true },
         }),
     ],
+    optimizeDeps: {
+        include: [
+            "@inertiajs/inertia",
+            "@inertiajs/inertia-svelte",
+            // "@inertiajs/progress",
+        ],
+    },
 });
